93a42cba878090c27acfbb4f85d0d245ff32b2ed
Ada Puric <puric.ada1@gmail.com>
2021-11-09 15:44:13 +0000

    second challenge task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # listing all tasks
+  get 'tasks', to: 'tasks#index'
+  # creating a new task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # going to a specific task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks', to: 'tasks#create'
+  # Delete a restaurant
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
